44af58c830e4bc7277e3d902cb07ca79f354c3a8
Jayeshwar Karthick V <jayesh-17090@jayesh-17090.csez.zohocorpin.com>
2024-11-10 22:39:40 +0530

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,129 @@
+function setInitialSongData(){
+    currentPlayList = [];
+    songContainer.textContent = "";
+    songList.forEach(song => {
+        addSongElem(song);
+    });
+}
+
+function filterByGenre(genre){
+    currentPlayList = [];
+    songContainer.textContent = "";
+    songList.forEach(song => {
+        if(song.genre == genre){
+            addSongElem(song);
+        }
+    });
+}
+
+function addSongElem(song){
+    let addSong = JSON.parse(JSON.stringify(song));
+    addSong.id = currentPlayList.length;
+    currentPlayList.push(addSong);
+    const songElem = document.createElement('div');
+    songElem.classList.add('song');
+    songElem.setAttribute('data-id', addSong.id);
+    songElem.textContent = `${addSong.name} - ${addSong.artist}`;
+    songElem.addEventListener('click', ()=> {
+        let songId = songElem.getAttribute('data-id');
+        playSong(songId);
+    })
+    songContainer.appendChild(songElem);
+}
+
+function playSong(songId){
+    let songSelected;
+
+    for(let song of currentPlayList){
+        if(song.id == songId){
+            songSelected = song;
+            break; 
+        }
+    }
+
+    if(currentSongPlaying){
+        currentSongPlaying.style.backgroundColor = 'rgb(35, 178, 244)';
+        currentSongPlaying.style.color = 'black';
+    }
+
+    for(let songElem of songListContainer.querySelectorAll('.song')){
+        if (songElem.getAttribute('data-id') == songId) {
+            songElem.style.backgroundColor = 'rgb(0 97 141)';
+            songElem.style.color = 'white';
+            songElem.classList.add('song-playing');
+            currentSongPlaying = songElem;
+            break;
+        };
+    }
+
+    imgElem.setAttribute('src', songSelected.image);
+    imgElem.setAttribute('alt', songSelected.name);
+    songNameElem.textContent = songSelected.name;
+    artistNameElem.textContent = `-${songSelected.artist}`;
+
+    audioSrcElem.setAttribute('src', songSelected.source);
+    audioSrcElem.setAttribute('type', songSelected.srcType);
+
+    audioElem.load();
+
+    // Optionally, start playing the song
+    audioElem.play().catch(error => {
+        console.error("Error playing audio:", error);
+    });
+}
+
+function playPrevSong(){
+    let currentSongId = currentSongPlaying.getAttribute('data-id');
+    let prevSongId = (parseInt(currentSongId) - 1).toString();
+    prevSongId = prevSongId < 0 ? (currentPlayList.length - 1).toString() : prevSongId;
+    playSong(prevSongId);
+}
+
+function playNextSong(){
+    let currentSongId = currentSongPlaying.getAttribute('data-id');
+    let nextSongId = (parseInt(currentSongId) + 1).toString();
+    nextSongId = nextSongId >= currentPlayList.length ? "0": nextSongId;
+    playSong(nextSongId);
+}
+
+
+const songContainer = document.querySelector('#song-list');
+const filterSongSelect = document.getElementById('filter-song');
+const cardElem = document.querySelector('.card');
+const imgElem = cardElem.querySelector('img');
+
+const songListContainer = document.querySelector('.song-list');
+const songNameElem = cardElem.querySelector('.song-name');
+const artistNameElem = cardElem.querySelector('.artist-name');
+
+const audioContainer =  document.querySelector('.audio-action');
+const audioElem = audioContainer.querySelector('audio'); // Select the audio element directly
+const audioSrcElem = audioElem.querySelector('source');
+
+const songButtonContainer = document.querySelector('.song-btn');
+const prevSongBtn = songButtonContainer.querySelector('.prev');
+const addToPlayListBtn = songButtonContainer.querySelector('.add-to-playlist');
+const nextSongBtn = songButtonContainer.querySelector('.next');
+
+
+let currentSongPlaying;
+let currentPlayList = songList
+
+filterSongSelect.addEventListener('change', () => {
+    let selectedGenre = filterSongSelect.value;
+    if(selectedGenre == 'all'){
+        setInitialSongData();
+    } else {
+        filterByGenre(selectedGenre);
+    }
+});
+
+prevSongBtn.addEventListener('click', () => {
+    playPrevSong();
+})
+
+nextSongBtn.addEventListener('click', () => {
+    playNextSong();
+})
+
+setInitialSongData();
---
+++ scripts/songObject.js
@@ -0,0 +1,118 @@
+const songList = [
+    {
+        id: "0",
+        name: "Everybody Get Up",
+        artist: "Everet Almond",
+        image: "media/img1.webp",
+        genre: "rock",
+        source: "songs/Everybody Get Up - Everet Almond.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "1",
+        name: "Red Whine",
+        artist: "Everet Almond",
+        image: "media/img2.webp",
+        genre: "rock",
+        source: "songs/Red Whine - Everet Almond.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "2",
+        name: "Every Night Of The Week",
+        artist: "Everet Almond",
+        image: "media/img3.webp",
+        genre: "rock",
+        source: "songs/Every Night Of The Week - Everet Almond.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "3",
+        name: "Rain Rain Go Away",
+        artist: "Everet Almond",
+        image: "media/img4.jpeg",
+        genre: "rock",
+        source: "songs/Rain Rain Go Away - Everet Almond.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "4",
+        name: "Future Freeway",
+        artist: "Adam MacDougall",
+        image: "media/img1.webp",
+        genre: "pop",
+        source: "songs/Future Freeway - Adam MacDougall.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "5",
+        name: "Life on Hold",
+        artist: "Everet Almond",
+        image: "media/img2.webp",
+        genre: "pop",
+        source: "songs/Life on Hold - Everet Almond.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "6",
+        name: "Toy Shopping",
+        artist: "Jeremy Korpas",
+        image: "media/img3.webp",
+        genre: "pop",
+        source: "songs/Toy Shopping - Jeremy Korpas.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "7",
+        name: "Forever Friends",
+        artist: "Jeremy Korpas",
+        image: "media/img4.jpeg",
+        genre: "pop",
+        source: "songs/Forever Friends - Jeremy Korpas.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "8",
+        name: "Shady",
+        artist: "Density & Time",
+        image: "media/img1.webp",
+        genre: "hip-hop",
+        source: "songs/Shady - Density & Time.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "9",
+        name: "Bump",
+        artist: "Dyalla",
+        image: "media/img2.webp",
+        genre: "hip-hop",
+        source: "songs/Bump - Dyalla.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "10",
+        name: "Good Energy",
+        artist: "Dyalla",
+        image: "media/img3.webp",
+        genre: "hip-hop",
+        source: "songs/Good Energy - Dyalla.mp3",
+        srcType: "audio/mpeg"
+    },
+    {
+        id: "11",
+        name: "Sofa",
+        artist: "Dyalla",
+        image: "media/img4.jpeg",
+        genre: "hip-hop",
+        source: "songs/Sofa - Dyalla.mp3",
+        srcType: "audio/mpeg"
+    },
+]    
+
+const trackSongs = [
+    {
+        prev: "",
+        current: "",
+        next: ""
+    }
+];
